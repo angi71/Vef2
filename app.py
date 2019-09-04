829c848e2c4d36981a28ea44c934ab5748ef1886
angi71 <angi80086@gmail.com>
2019-09-04 16:41:11 +0000

    ok

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    #the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
     <p><a href="/sida2" title="Síða 2">Síða 2 </a> | <a href="/sida3" title="Síða 3">Síða 3 </a></p>
     <img src="http://loremflickr.com/600/400" />
-    """ #.format(time=the_time)
+    """ .format(time=the_time)
 
 @app.route('/sida2')
 def page2():
@@ -32,5 +32,5 @@ def page3():
     """ 
 
 if __name__ == '__main__':
-    #app.run()
-    app.run(debug=True, use_reloader=True)
+    app.run()
+    #app.run(debug=True, use_reloader=True)
